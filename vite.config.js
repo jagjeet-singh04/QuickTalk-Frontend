b20f087d97c99a31b7277a18bb7df3932233cf9e
Jagjeet <jagjeetsingh0424@gmail.com>
2025-06-20 03:00:35 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()] , 
-  
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'socket.io-client', 'zustand'],
+          tailwind: ['tailwindcss', '@tailwindcss/vite', 'autoprefixer']
+        }
+      }
+    }
+  },
   
 })
