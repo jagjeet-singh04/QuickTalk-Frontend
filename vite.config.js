7d2e3d9528a9217ad479db29b75bdf3db08d7aea
Jagjeet <jagjeetsingh0424@gmail.com>
2025-06-20 03:34:55 +0530

    "final changes"

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()] , 
+  plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: {
           react: ['react', 'react-dom'],
-          vendor: ['axios', 'socket.io-client', 'zustand'],
-          tailwind: ['tailwindcss', '@tailwindcss/vite', 'autoprefixer']
+          vendor: ['axios', 'socket.io-client', 'zustand']
         }
       }
     }
   },
-  
-})
+  server: {
+    port: 3000
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  }
+});
