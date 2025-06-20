13e3ae64bd4503a05eb3b49e6b06187698c8e4f2
Jagjeet <jagjeetsingh0424@gmail.com>
2025-06-21 00:21:05 +0530

    axios error

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://quic-talk-backend.vercel.app",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://quictalk-backend-production.up.railway.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
